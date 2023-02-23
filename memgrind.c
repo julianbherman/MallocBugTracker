4f13a1c7146928ae4ba5a84daa0f44f6867ddc4c
Giyuu <tranhoanglong1904@gmail.com>
2023-02-22 22:05:43 -0500

    add test4

--- memgrind.c
+++ memgrind.c
@@ -99,6 +99,64 @@ void test3(){
     printf("The average time is: \n");
     print_time_double(average_time/(double)repeat);   
 }
+void test4(){
+    // Fill up 4064 bytes of memory; Malloc and free 120 time
+    struct timeval start, end;
+    char *arr[260], *p;
+    int repeat = 50;
+    long average_time = 0;
+
+    for (int run = 0; run < repeat; run++)
+    {
+        for (int i = 0; i < 254; i++) 
+        {
+            p = (char*) malloc(sizeof(char)*1);
+            arr[i] = p;
+        }
+        gettimeofday(&start, NULL);
+        for (int i = 0; i < 120; i++)
+        {
+            p = (char*) malloc(sizeof(char)*1);
+            free(p);   
+        }  
+        gettimeofday(&end, NULL);
+        for (int i = 0; i < 254; i++) 
+            free(arr[i]);
+        average_time += count_time(start,end);
+        //print_time(count_time(start,end));
+    }
+    printf("The average time is: \n");
+    print_time_double(average_time/(double)repeat);   
+}
+void test5(){
+    // Fill up 4064 bytes of memory; Malloc and free 120 time
+    struct timeval start, end;
+    char *arr[260], *p;
+    int repeat = 50;
+    long average_time = 0;
+
+    for (int run = 0; run < repeat; run++)
+    {
+        for (int i = 0; i < 254; i++) 
+        {
+            p = (char*) malloc(sizeof(char)*1);
+            arr[i] = p;
+        }
+        gettimeofday(&start, NULL);
+        for (int i = 0; i < 120; i++)
+        {
+            p = (char*) malloc(sizeof(char)*1);
+            free(p);   
+        }  
+        gettimeofday(&end, NULL);
+        for (int i = 0; i < 254; i++) 
+            free(arr[i]);
+        average_time += count_time(start,end);
+        //print_time(count_time(start,end));
+    }
+    printf("The average time is: \n");
+    print_time_double(average_time/(double)repeat);   
+}
 int main(){
     printf("TEST 1: Malloc and immediately free 120 times\n");
     test1();
@@ -106,6 +164,9 @@ int main(){
     test2();
     printf("TEST 3: Randomly choose between Malloc and Free until Malloc() 120 times, then free all\n");
     test3(); 
+    printf("TEST 4: Fill up memory 4064 bytes; malloc and free 120 times at the end of memory\n");
+    test4();
+    printf("")
     printf("\n\n");
     return 0;
 }
